1693efd2f9fc497611f087c6abc5a9c208627c42
dkirby-ms <saitcho@outlook.com>
2024-09-18 14:11:02 -0500

    add aio setup script

---
+++ infrastructure/artifacts/setupAIO.sh
@@ -0,0 +1,16 @@
+
+
+az config set extension.use_dynamic_install=yes_without_prompt
+
+az connectedk8s enable-features --name $arcClusterName --resource-group $resourceGroup --features cluster-connect custom-locations --custom-locations-oid $customLocationRPOID
+
+az extension add --upgrade --name azure-iot-ops
+
+echo fs.inotify.max_user_instances=8192 | sudo tee -a /etc/sysctl.conf
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
+
+sudo sysctl -p
+
+echo fs.file-max = 100000 | sudo tee -a /etc/sysctl.conf
+
+sudo sysctl -p
