f84d2c35787ab8a87e620106017837ba0b5d77b3
dkirby-ms <saitcho@outlook.com>
2024-09-19 12:57:45 -0500

    updates to AIO deployment

--- infrastructure/artifacts/setupAIO.sh
+++ infrastructure/artifacts/setupAIO.sh
@@ -1,8 +1,10 @@
 # customLocationRPOID=$(az ad sp list --filter "displayname eq 'Custom Locations RP'" --query "[?appDisplayName=='Custom Locations RP'].id" -o tsv) # needs to be run by account with Directory.Read perms on Entra tenant
 customLocationRPOID="d7c8af5d-5320-435c-8d61-88d4cc0f345d" # for Azure Stack Infra Entra tenant
 resourceGroup="JumpstartCerebral"
-arcClusterName=$(az connectedk8s list -g Cerebral --query [].name -o tsv)
+arcClusterName=$(az connectedk8s list -g $resourceGroup --query [].name -o tsv)
 kvId=$(az keyvault list --resource-group $resourceGroup --query "[].id" -o tsv)
+kvName=$(az keyvault list --resource-group $resourceGroup --query "[].name" -o tsv)
+kvString="https://$kvName.vault.azure.net"
 #oid=$()
 
 az config set extension.use_dynamic_install=yes_without_prompt
@@ -21,6 +23,8 @@ echo fs.file-max = 100000 | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
 
-az keyvault set-policy --secret-permissions all --key-permissions all --name $kvName -- --resource-group $resourceGroup
+#az keyvault set-policy --secret-permissions all --key-permissions all --name $kvName -- --resource-group $resourceGroup
+appid=$(az keyvault secret show --id $kvString/secrets/SPNAppId --query value -o tsv)
+appsecret=$(az keyvault secret show --id $kvString/secrets/SPNSecret --query value -o tsv)
+az iot ops init --cluster $arcClusterName --resource-group $resourceGroup --kv-id $kvId --mq-insecure --sp-app-id $appid --sp-secret $appsecret
 
-az iot ops init --cluster $arcClusterName --resource-group $resourceGroup --kv-id $kvId --mq-insecure
